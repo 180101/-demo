4106e04a35776dc1533a09ae9261c6890c754829
Leslie Jude <2719502607@qq.com>
2019-10-07 20:13:40 +0800

    git is established

---
+++ 答题demo/GitHelper.m
@@ -0,0 +1,33 @@
+//
+//  GitHelper.m
+//  答题demo
+//
+//  Created by Jude Leslie on 2019/10/7.
+//  Copyright © 2019 Jude Leslie. All rights reserved.
+//
+
+#import "GitHelper.h"
+
+@interface GitHelper ()
+
+@end
+
+@implementation GitHelper
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    NSLog(@"Git is established");
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
